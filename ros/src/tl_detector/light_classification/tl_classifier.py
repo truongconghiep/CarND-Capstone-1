a6e5ce775c9e8cda7877565cb5773ec7349b55cb
truongconghiep <truongconghiep@gmail.com>
2019-06-09 17:04:46 +0200

    added new opencv traffic light detection algorithm

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -19,33 +19,21 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        result = TrafficLight.UNKNOWN
         HsvImg = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         
-        red1 = cv2.inRange(HsvImg, np.array([0,50,50]) , np.array([10,255,255]))
-        red2 = cv2.inRange(HsvImg, np.array([170,50,50]) , np.array([180,255,255]))
-        converted_img_red = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
-        blur_img = cv2.GaussianBlur(converted_img_red,(15,15),0)
-        circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
-
-        yellow1 = cv2.inRange(HsvImg, np.array([22, 100, 160]) , np.array([34, 255, 255]))
-        yellow2 = cv2.inRange(HsvImg, np.array([5, 50, 50]) , np.array([15, 255, 255]))
-        converted_img_yellow = cv2.addWeighted(yellow1, 1.0, yellow2, 1.0, 0.0)
-        blur_img_yellow = cv2.GaussianBlur(converted_img_yellow,(15,15),0)
-        circles_yellow = cv2.HoughCircles(blur_img_yellow,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
-
-        #Green1 = cv2.inRange(HsvImg, np.array([30, 59, 50]) , np.array([79, 158, 171]))
-        #Green2 = cv2.inRange(HsvImg, np.array([0, 255, 0]) , np.array([229, 255, 204]))
-        #converted_img_green = cv2.addWeighted(Green1, 1.0, Green2, 1.0, 0.0)
-        #blur_img_green = cv2.GaussianBlur(converted_img_green,(15,15),0)
-        #circles_green = cv2.HoughCircles(blur_img_green,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=150)
-        
-        if circles is not None:
+        frame_threshed = cv2.inRange(HsvImg, np.array([0, 120, 120],np.uint8), np.array([10, 255, 255],np.uint8))
+        r = cv2.countNonZero(frame_threshed)
+        if r > 50:
             return TrafficLight.RED
-        if circles_yellow is not None:
-            return TrafficLight.YELLOW
-        #if circles_green is not None:
-        #    return TrafficLight.GREEN
-            #rospy.logwarn("state Yellow {0}".format(0))
 
-        return result
+        frame_threshed = cv2.inRange(HsvImg, np.array([28, 120, 120],np.uint8), np.array([43, 255, 255],np.uint8))
+        y = cv2.countNonZero(frame_threshed)
+        if y > 50:
+            return TrafficLight.YELLOW
+
+        frame_threshed = cv2.inRange(HsvImg, np.array([64, 120, 120],np.uint8), np.array([99, 255, 255],np.uint8))
+        g = cv2.countNonZero(frame_threshed)
+        if g > 50:
+            return TrafficLight.GREEN
+
+        return TrafficLight.UNKNOWN
