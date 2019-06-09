65e50000f7383fbd67d62ee91b19a2386dda0979
truongconghiep <truongconghiep@gmail.com>
2019-06-09 18:32:47 +0200

    disabled log message

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -160,7 +160,7 @@ class TLDetector(object):
         
         if closest_light:
             state = self.get_light_state(closest_light)
-            rospy.logwarn("state {0}".format(state))
+            #rospy.logwarn("state {0}".format(state))
             return line_wp_idx, state
 
         return -1, TrafficLight.UNKNOWN
